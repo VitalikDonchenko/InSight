90dd71eab4e9e2cf1587b46ac611672df0519243
VITALIK <cool.donchenko@mail.ru>
2020-08-27 12:56:35 +0300

    Add readme

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 
-app.listen(process.env.PORT);
-// app.listen(process.env.PORT ?? 3000);
+// app.listen(process.env.PORT);
+app.listen(process.env.PORT ?? 3000);
